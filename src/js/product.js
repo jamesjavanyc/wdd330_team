6ebbed3f4794855ef14cfb48e61a1ff355a04b23
jamesjavanyc` <1931736857@qq.com>
2022-09-19 23:25:08 -0400

    requirements

--- src/js/product.js
+++ src/js/product.js
@@ -1,46 +1,47 @@
 import { getLocalStorage } from "./utils.js";
 import ProductData from "./productData.js"
+import {getParam} from "./utils.js"
+import {ProductDetails} from "./productDetails.js"
 
-let productData = new ProductData();
-console.log(productData)
+let dataSource;
+const productId = getParam("product");
+let productDetail;
 
-
-
-let products = [];
-function convertToJson(res) {
-  if (res.ok) {
-    return res.json();
-  } else {
-    throw new Error("Bad Response");
-  }
+async function initDataSource(){
+  dataSource = new ProductData();
+  await dataSource.getData();
+  productDetail = new ProductDetails(productId, dataSource);
+  await productDetail.init()
 }
+initDataSource();
 
-function setLocalStorage(key, data) {
-  localStorage.setItem(key, JSON.stringify(data));
-}
+
+
+
+// let products = [];
+// function convertToJson(res) {
+//   if (res.ok) {
+//     return res.json();
+//   } else {
+//     throw new Error("Bad Response");
+//   }
+// }
+
 
 // get tents data
-function getProductsData() {
-  fetch("../json/tents.json")
-    .then(convertToJson)
-    .then((data) => {
-      products = data;
-    });
-}
 // or should we do it this way?
 // async function getProductsDataAwait() {
 //   products = await fetch("../json/tents.json").then(convertToJson);
 // }
 
 // add to cart button event handler
-function addToCart(e) {
-  const product = products.find((item) => item.Id === e.target.dataset.id);
-  let cart = getLocalStorage("so-cart");
-  if (cart == null) cart = [];
-  cart.push(product);
-  setLocalStorage("so-cart", cart);
-}
+// function addToCart(e) {
+//   const product = products.find((item) => item.Id === e.target.dataset.id);
+//   let cart = getLocalStorage("so-cart");
+//   if (cart == null) cart = [];
+//   cart.push(product);
+//   setLocalStorage("so-cart", cart);
+// }
 
-getProductsData();
-// add listener to Add to Cart button
-document.getElementById("addToCart").addEventListener("click", addToCart);
+// // add listener to Add to Cart button
+// document.getElementById("addToCart").addEventListener("click", addToCart);
--- src/js/productData.js
+++ src/js/productData.js
@@ -1,24 +1,25 @@
 import {getLocalStorage, setLocalStorage} from "./utils.js"
 
-
-
-
 export default  class ProductData{
-    constructor(){
-        this.products = this.getData();
+    constructor(tents){
+
     }
     async getData(){
         let data = await fetch("../json/tents.json").then(res => this.convertToJson(res));
-        console.log(data)
         this.products = data
         return data;
     }
-    findProductById(id){
-        const product = this.products.find((item) => item.Id === id);
-        let cart = getLocalStorage("so-cart");
-        if (cart == null) cart = [];
-        cart.push(product);
-        setLocalStorage("so-cart", cart);
+    async findProductById(id){
+        let product = null;
+        if(this.products == null){
+            await this.getData()
+        }
+        for(let item of this.products){
+            if(item.Id == id){
+                product = item
+            }
+        }
+        return product
     }
     convertToJson(res) {
         if (res.ok) {
