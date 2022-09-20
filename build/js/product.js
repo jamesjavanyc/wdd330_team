570d20230aa98f996c9fbcf0db16722978cea974
Mael1337 <12andrewmoreno@gmail.com>
2022-09-19 21:11:52 -0500

    changes

--- build/js/product.js
+++ build/js/product.js
@@ -1,5 +1,46 @@
-<<<<<<< HEAD
-import{getLocalStorage as a}from"./utils.js";let n=[];function s(t){if(t.ok)return t.json();throw new Error("Bad Response")}function c(t,e){localStorage.setItem(t,JSON.stringify(e))}function d(){fetch("../json/tents.json").then(s).then(t=>{n=t})}function i(t){const e=n.find(r=>r.Id===t.target.dataset.id);let o=a("so-cart");o==null&&(o=[]),o.push(e),c("so-cart",o)}d(),document.getElementById("addToCart").addEventListener("click",i);
-=======
-let products=[];function convertToJson(t){if(t.ok)return t.json();throw new Error("Bad Response")}function setLocalStorage(t,e){localStorage.setItem(t,JSON.stringify(e))}function getProductsData(){fetch("../json/tents.json").then(convertToJson).then(t=>{products=t})}function addToCart(t){const e=products.find(n=>n.Id===t.target.dataset.id);setLocalStorage("so-cart",e)}getProductsData(),document.getElementById("addToCart").addEventListener("click",addToCart);
->>>>>>> 61620455b33f8a17cc98c3502b7054622e7c39e6
+import { getLocalStorage } from "./utils.js";
+import ProductData from "./productData.js"
+
+let productData = new ProductData();
+console.log(productData)
+
+
+
+let products = [];
+function convertToJson(res) {
+  if (res.ok) {
+    return res.json();
+  } else {
+    throw new Error("Bad Response");
+  }
+}
+
+function setLocalStorage(key, data) {
+  localStorage.setItem(key, JSON.stringify(data));
+}
+
+// get tents data
+function getProductsData() {
+  fetch("../json/tents.json")
+    .then(convertToJson)
+    .then((data) => {
+      products = data;
+    });
+}
+// or should we do it this way?
+// async function getProductsDataAwait() {
+//   products = await fetch("../json/tents.json").then(convertToJson);
+// }
+
+// add to cart button event handler
+function addToCart(e) {
+  const product = products.find((item) => item.Id === e.target.dataset.id);
+  let cart = getLocalStorage("so-cart");
+  if (cart == null) cart = [];
+  cart.push(product);
+  setLocalStorage("so-cart", cart);
+}
+
+getProductsData();
+// add listener to Add to Cart button
+document.getElementById("addToCart").addEventListener("click", addToCart);
