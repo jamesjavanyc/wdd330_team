763049a78e3982cf5b422d0f0093ac9f81f34028
yamiriv1311 <riv21019@byui.edu>
2022-10-02 00:15:21 -0500

    Server was changed Team Act 5 Yami branch

--- src/js/productList.js
+++ src/js/productList.js
@@ -17,7 +17,7 @@ export default class ProductList {
   prepareTemplate(template, product) {
     
     template.querySelector("a").href +=  product.Id;
-    template.querySelector("img").src = product.Image;
+    template.querySelector("img").src = product.Images.PrimaryMedium;
     template.querySelector("img").alt += product.Name;
     template.querySelector(".card__brand").textContent = product.Brand.Name;
     template.querySelector(".card__name").textContent = product.NameWithoutBrand;
