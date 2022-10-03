b576c93da733d94fcbfe08404be785bb6539bd53
Mael1337 <12andrewmoreno@gmail.com>
2022-10-03 17:08:00 -0500

    update

--- src/js/product.js
+++ src/js/product.js
@@ -1,49 +1,9 @@
+import ExternalServices from "./ExternalServices.js";
+import ProductDetails from "./productDetails.js";
+import { getParam } from "./utils.js";
 
-import { getLocalStorage } from "./utils.js";
-import ProductData from "./productData.js"
-import {getParam} from "./utils.js"
-import {ProductDetails} from "./productDetails.js"
-
-let dataSource;
 const productId = getParam("product");
-let productDetail;
+const dataSource = new ExternalServices();
 
-async function initDataSource(){
-  dataSource = new ProductData();
-  await dataSource.getData();
-  productDetail = new ProductDetails(productId, dataSource);
-  await productDetail.init()
-}
-initDataSource();
-
-
-
-
-// let products = [];
-// function convertToJson(res) {
-//   if (res.ok) {
-//     return res.json();
-//   } else {
-//     throw new Error("Bad Response");
-//   }
-// }
-
-
-// get tents data
-// or should we do it this way?
-// async function getProductsDataAwait() {
-//   products = await fetch("../json/tents.json").then(convertToJson);
-// }
-
-// add to cart button event handler
-// function addToCart(e) {
-//   const product = products.find((item) => item.Id === e.target.dataset.id);
-//   let cart = getLocalStorage("so-cart");
-//   if (cart == null) cart = [];
-//   cart.push(product);
-//   setLocalStorage("so-cart", cart);
-// }
-
-// // add listener to Add to Cart button
-// document.getElementById("addToCart").addEventListener("click", addToCart);
-
+const product = new ProductDetails(productId, dataSource);
+product.init();
--- src/js/productDetails.js
+++ src/js/productDetails.js
@@ -1,40 +1,46 @@
-import {getLocalStorage, setLocalStorage} from "./utils.js"
-export class ProductDetails {
-    constructor(productId, dataSource) {
-        this.productId = productId;
-        this.product = {};
-        this.dataSource = dataSource;
+import { setLocalStorage, getLocalStorage, loadHeaderFooter } from "./utils.js";
+
+loadHeaderFooter();
+export default class ProductDetails {
+  constructor(productId, dataSource) {
+    this.productId = productId;
+    this.product = {};
+    this.dataSource = dataSource;
+  }
+  async init() {
+    this.product = await this.dataSource.findProductById(this.productId);
+    document.querySelector("main").innerHTML = this.renderProductDetails();
+    // add listener to Add to Cart button
+    document
+      .getElementById("addToCart")
+      .addEventListener("click", this.addToCart.bind(this));
+  }
+  addToCart() {
+    // to fix the cart we need to get anything that is in the cart already.
+    let cartContents = getLocalStorage("so-cart");
+    //check to see if there was anything there
+    if (!cartContents) {
+      cartContents = [];
     }
-    async init(){
-      this.productDetail = await this.dataSource.findProductById(this.productId)
-      document.querySelector(".divider").innerHTML = this.renderProductDetails(this.productDetail);
-      document.getElementById("addToCart").addEventListener("click", this.addToCart.bind(this));
-    }
-    addToCart() {
-      let cart = getLocalStorage("so-cart");
-      if (cart == null) cart = [];
-      cart.push(this.productDetail);
-      setLocalStorage("so-cart", cart);
-    }
-    renderProductDetails(detail) {
-      console.log(detail)
-        const item =  `
-        <section class="product-detail">
-          <h3>${detail.Brand.Name}</h3>
-          <h2 class="divider">${detail.NameWithoutBrand}</h2>
-          <img class="divider"
-              src="${detail.Image}"
-              alt="Talus Tent - 4-Person, 3-Season" />
-          <p class="product-card__price">${detail.ListPrice}</p>
-          <p class="product__color">${detail.Colors}</p>
-          <p class="product__description">
-              ${detail.DescriptionHtmlSimple}
-          </p>
-          <div class="product-detail__add">
-              <button id="addToCart" data-id="989CG">Add to Cart</button>
-          </div>
-      </section>`;
-      return item;
-    }
-  
+    // then add the current product to the list
+    cartContents.push(this.product);
+    setLocalStorage("so-cart", cartContents);
+  }
+  renderProductDetails() {
+    return `<section class="product-detail"> <h3>${this.product.Brand.Name}</h3>
+    <h2 class="divider">${this.product.NameWithoutBrand}</h2>
+    <img
+      class="divider"
+      src="${this.product.Images.PrimaryLarge}"
+      alt="${this.product.NameWithoutBrand}"
+    />
+    <p class="product-card__price">$${this.product.FinalPrice}</p>
+    <p class="product__color">${this.product.Colors[0].ColorName}</p>
+    <p class="product__description">
+    ${this.product.DescriptionHtmlSimple}
+    </p>
+    <div class="product-detail__add">
+      <button id="addToCart" data-id="${this.product.Id}">Add to Cart</button>
+    </div></section>`;
+  }
 }
